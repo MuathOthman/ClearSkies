42be4cc5911a309598b6c1ec9b5ffa08710a77af
Agrinsadon <111856786+Agrinsadon@users.noreply.github.com>
2022-11-27 18:07:20 +0200

    New column in game called money. in phyton i made a command when you reach a new saatila it will add 50$ to your moneybudget

--- Testing/Agrin/TASK1/PYTHON/main.py
+++ Testing/Agrin/TASK1/PYTHON/main.py
@@ -1,12 +1,12 @@
 def moneybudget(nimi):
-    budget = 50     #The amount of money will add if you get a new saatila
-    budget1 = str(budget)
-    sql = "UPDATE game set money= '" + budget1 + "'"    # Adding the amount into sql
-    sql += "Where screen_name = '" + nimi + "'"
+    budget = "50"    #The amount of money will add if you get a new saatila
+    sql = "update game set money = money + '" + budget + "'"        # Adding the amount into sql
+    sql += " WHERE screen_name= '" + nimi + "'"
     kursori = yhteys.cursor()
     kursori.execute(sql)
     tulos = kursori.fetchall()
     return
 
-
 moneybudget(nimi) # This will go to function saa_tila when a new saatila is reached.
+
+#mysql commands ALTER TABLE game add money int;
