bebf0a62a28778348d8b731cd6e5aaca15f64e23
Agrinsadon <111856786+Agrinsadon@users.noreply.github.com>
2022-11-30 00:52:03 +0200

    login added data from mysql using python :p

---
+++ Testing/Agrin/TASK 4 (LOGIN)/log.py
@@ -0,0 +1,41 @@
+from flask import Flask
+import mysql.connector
+from flask_cors import CORS
+
+
+def connect_db():
+    return mysql.connector.connect(
+        host='127.0.0.1',
+        port=3306,
+        database='Flight_game',
+        user='root',
+        password='kirkuk123',
+        autocommit=True
+    )
+
+
+def olduser(name):
+    sql = f"select screen_name from game where screen_name= '{name}'"
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    result_set = cursor.fetchone()
+    if cursor.rowcount > 0:
+        return {"correct": result_set[0]}
+    else:
+        return {"Error": "Give me a correct name"}
+
+
+connection = connect_db()
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+
+@app.route('/login/<name>')
+def login(name):
+    response = olduser(name)
+    return response
+
+
+if __name__ == '__main__':
+    app.run(use_reloader=True, host='127.0.0.1', port=5998)
