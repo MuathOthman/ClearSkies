c95dbd499cbbd62c978efb6e891ba5d754d5d040
isakovero <111856755+isakovero@users.noreply.github.com>
2022-11-30 23:54:41 +0200

    Print Co2-budget to HTML#7

--- JAVASCRIPT/money-budget.js
+++ JAVASCRIPT/money-budget.js
@@ -1,1 +1,22 @@
-/* Isan koodi */
+
+async function budget(name){
+  const response = await fetch('http://127.0.0.1:5000/co2_budget/' + name);
+  console.log('response', response)
+  const data = await response.json()
+  console.log('data', data);
+  return data
+}
+
+function renderHTML(data) {
+    const p = document.getElementById('consumed');
+    p.innerText = data['co2_budget'] + 'kg';
+}
+
+async function main() {
+  let icaoInput = prompt('Mikä kenttä (ICAO)?');
+  const aiportData = await budget(icaoInput);
+  console.log('Ariport data:', aiportData);
+  renderHTML(aiportData);
+}
+  main()
+  console.log('Ohjelma jatkuu');
--- PYTHON/money-budget.py
+++ PYTHON/money-budget.py
@@ -1,1 +1,36 @@
-#Isan koodi
+import mysql.connector
+from flask import Flask
+from flask_cors import CORS
+
+def connect_db():
+    return mysql.connector.connect(
+        host='127.0.0.1',
+        port=3306,
+        database='lentopeli',
+        user='root',
+        password='',
+        autocommit=True
+    )
+
+
+def budget(name):
+    sql = f"select co2_budget from game where screen_name ='{name}'"
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    result_set = cursor.fetchone()
+    if cursor.rowcount > 0:
+        return {"co2_budget": result_set[0]}
+    else:
+        return {"Error": "Give me a correct name"}
+
+connection = connect_db()
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+@app.route('/co2_budget/<name>')
+def co2budget(name):
+    response = budget(name)
+    return response
+
+if __name__ == '__main__':
+    app.run(use_reloader=True, host='127.0.0.1', port=5000)
